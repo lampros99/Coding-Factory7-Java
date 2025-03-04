0ae22cf51750f983843a45d0c6711573be0c6cf5
lampros alexandris <lamprospatmos19@gmail.com>
2025-03-04 10:14:45 +0200

    feat: Swing, JFrames and Events created

--- src/gr/aueb/cf/ch22_swingfrontend/HelloFrame.java
+++ src/gr/aueb/cf/ch22_swingfrontend/HelloFrame.java
@@ -1,2 +1,29 @@
-package gr.aueb.cf.ch22_swingfrontend;public class HelloFrame {
+package gr.aueb.cf.ch22_swingfrontend;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class HelloFrame extends JFrame {
+
+    public HelloFrame(){
+        this.setTitle("Hello coding Frame");
+        setSize(400, 200);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        JPanel contentPane = new JPanel();
+        setContentPane(contentPane);
+        contentPane.add(new JLabel("Coding Factory"));
+
+        JButton btn = new JButton("Click me");
+        contentPane.add(btn);
+
+        btn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JOptionPane.showMessageDialog(null,"Hello Coding Factory", "Hello",
+                        JOptionPane.INFORMATION_MESSAGE);
+            }
+        });
+    }
 }
--- src/gr/aueb/cf/ch22_swingfrontend/HelloSwing.java
+++ src/gr/aueb/cf/ch22_swingfrontend/HelloSwing.java
@@ -1,2 +1,24 @@
-package gr.aueb.cf.ch22_swingfrontend;public class HelloSwing {
+package gr.aueb.cf.ch22_swingfrontend;
+
+import javax.swing.*;
+
+
+public class HelloSwing {
+
+    public static void main(String[] args) {
+        JFrame jframe = new JFrame();
+
+        jframe.setTitle("First Swing app");
+        jframe.setSize(600, 300);
+        jframe.setLocationRelativeTo(null); //aligns to the center of the screen
+        jframe.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+
+
+        JPanel contentPane = new JPanel();
+        jframe.setContentPane(contentPane);
+
+        contentPane.add(new JLabel("Hello World"));
+        jframe.setVisible(true);
+
+    }
 }
