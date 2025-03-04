c4da54f93ef2fb3c39d6b7994c1839673843ea60
lampros alexandris <lamprospatmos19@gmail.com>
2025-03-04 10:11:55 +0200

    updates

---
+++ src/gr/aueb/cf/ch14/services/HelloUtil.java
@@ -0,0 +1,15 @@
+package gr.aueb.cf.ch14.services;
+
+/**
+ * No instances of this class should be available.
+ */
+public class HelloUtil {
+
+    private HelloUtil(){
+
+    }
+
+    public static void sayHello(){
+        System.out.println("Hello Coding!");
+    }
+}
